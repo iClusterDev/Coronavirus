32b3ac9637febf6b0beb8b3779dbb8ffbe649360
iClusterDev <cluster.coredev@gmail.com>
2020-04-07 20:19:56 +0100

    server updated with new data structure

--- server/schema.js
+++ server/schema.js
@@ -1,88 +1,55 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLFloat } = require("graphql");
 const axios = require("axios");
-const Data = require("./data");
+const getData = require("./tools");
 
 const CountryType = new GraphQLObjectType({
   name: "Country",
   fields: () => ({
-    country: { type: GraphQLString },
-    cases: { type: GraphQLInt },
-    deaths: { type: GraphQLInt },
-    active: { type: GraphQLInt },
-    critical: { type: GraphQLInt },
-    recovered: { type: GraphQLInt },
-    todayCases: { type: GraphQLInt },
-    todayDeaths: { type: GraphQLInt },
-    casesPerOneMillion: { type: GraphQLInt }
-  })
-});
-
-const HistoryType = new GraphQLObjectType({
-  name: "History",
-  fields: () => ({
-    country: { type: GraphQLString },
-    timeline: { type: TimelineType }
-  })
+    name: { type: GraphQLString },
+    lat: { type: GraphQLFloat },
+    long: { type: GraphQLFloat },
+    flag: { type: GraphQLString },
+    timeline: { type: TimelineType },
+  }),
 });
 
 const TimelineType = new GraphQLObjectType({
   name: "Timeline",
   fields: () => ({
-    cases: { type: new GraphQLList(DataType) },
-    deaths: { type: new GraphQLList(DataType) },
-    recovered: { type: new GraphQLList(DataType) }
-  })
+    cases: { type: new GraphQLList(TimelineItemType) },
+    deaths: { type: new GraphQLList(TimelineItemType) },
+    recovered: { type: new GraphQLList(TimelineItemType) },
+  }),
 });
 
-const DataType = new GraphQLObjectType({
+const TimelineItemType = new GraphQLObjectType({
   name: "Data",
   fields: () => ({
     date: { type: GraphQLString },
     total: { type: GraphQLInt },
     today: { type: GraphQLInt },
-    growth: { type: GraphQLFloat }
-  })
+    growth: { type: GraphQLFloat },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    history: {
-      type: new GraphQLList(HistoryType),
-      args: { countries: { type: new GraphQLList(GraphQLString) } },
+    country: {
+      type: CountryType,
+      args: { name: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`https://corona.lmao.ninja/v2/historical/${args.countries}`).then(res => {
-          if (Array.isArray(res.data)) {
-            return res.data.map(item => {
-              return new Data(item);
-            });
-          }
-          return [new Data(res.data)];
+        const promises = [
+          `https://corona.lmao.ninja/countries/${args.name}`,
+          `https://corona.lmao.ninja/yesterday/${args.name}`,
+          `https://corona.lmao.ninja/v2/historical/${args.name}?lastdays=all`,
+        ].map((url) => axios.get(url, { timeout: 500 }).then((res) => res.data));
+        return Promise.all(promises).then(([res1, res2, history]) => {
+          return getData(res1, res2, history);
         });
-      }
+      },
     },
-    countries: {
-      type: new GraphQLList(CountryType),
-      resolve(parentValue, args) {
-        return axios.get(`https://corona.lmao.ninja/countries/`).then(res => res.data);
-      }
-    },
-    stats: {
-      type: new GraphQLList(CountryType),
-      args: { countries: { type: new GraphQLList(GraphQLString) } },
-      resolve(parentValue, args) {
-        return axios.get(`https://corona.lmao.ninja/countries/`).then(res => {
-          return res.data.filter(item => {
-            for (const country of args.countries) {
-              if (item.country.toLowerCase() === country.toLowerCase()) {
-                return item;
-              }
-            }
-          });
-        });
-      }
-    }
-  }
+  },
 });
 
 module.exports = new GraphQLSchema({ query: RootQuery });
---
+++ server/tools.js
@@ -0,0 +1,78 @@
+const moment = require("moment");
+
+const summarize = (sourceObj) => {
+  const outputArray = [];
+  Object.entries(sourceObj).forEach(([key, value], index, source) => {
+    let growth = 0;
+    let today = 0;
+    if (index > 2) {
+      // this is on the total values
+      // const present = value;
+      // const past = source[index - 1][1];
+      // const net = present - past;
+      // const rate = past === 0 ? 0 : net / past;
+      // growth = rate * 100;
+      // today = net;
+      // this is on the today's values
+      const present = value - source[index - 1][1];
+      const past = source[index - 1][1] - source[index - 2][1];
+      const net = present - past;
+      const rate = past === 0 ? 0 : net / past;
+      growth = rate * 100;
+      today = present;
+    }
+    outputArray.push({ date: key, total: value, today, growth });
+  });
+  return outputArray;
+};
+
+const getTimeline = (history) => {
+  const { timeline: { cases, deaths, recovered } = {} } = history;
+  return {
+    cases: summarize(cases),
+    deaths: summarize(deaths),
+    recovered: summarize(recovered),
+  };
+};
+
+const getLastValidUpdate = (res1, res2) => {
+  let { cases: res1_cases, todayCases: res1_todayCases } = res1;
+  let { cases: res2_cases, todayCases: res2_todayCases } = res2;
+  res1.isNew = true;
+  res2.isNew = false;
+  if (res1_cases === res2_cases) {
+    return res1_todayCases === res2_todayCases ? { ...res1 } : { ...res2 };
+  } else {
+    return { ...res1 };
+  }
+};
+
+const getCountryInfo = (lastValidUpdate) => {
+  const { lat, long, flag } = lastValidUpdate.countryInfo;
+  const { country: name } = lastValidUpdate;
+  return {
+    name,
+    lat,
+    long,
+    flag,
+  };
+};
+
+const getUpdatedHistory = (history, lastValidUpdate) => {
+  if (lastValidUpdate.isNew) {
+    const date = moment(parseInt(lastValidUpdate.updated)).format("M/D/YY");
+    const { cases, deaths, recovered } = lastValidUpdate;
+    history.timeline.cases[date] = cases;
+    history.timeline.deaths[date] = deaths;
+    history.timeline.recovered[date] = recovered;
+  }
+  return { ...history };
+};
+
+module.exports = (res1, res2, history) => {
+  const lastValidUpdate = getLastValidUpdate(res1, res2);
+  const updatedHistory = getUpdatedHistory(history, lastValidUpdate);
+  const countryInfo = getCountryInfo(lastValidUpdate);
+  const timeline = getTimeline(updatedHistory);
+  return { ...countryInfo, timeline };
+};
