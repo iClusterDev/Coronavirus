03a74425ffb7e8d7aaf18569792259c5cf5c2d46
iClusterDev <cluster.coredev@gmail.com>
2020-04-12 18:51:55 +0100

    fixed the store actions

--- server/tools.js
+++ server/tools.js
@@ -5,21 +5,36 @@ const summarize = (sourceObj) => {
   Object.entries(sourceObj).forEach(([key, value], index, source) => {
     let growth = 0;
     let today = 0;
-    if (index > 2) {
+    if (index > 0) {
       // this is on the total values
-      // const present = value;
-      // const past = source[index - 1][1];
-      // const net = present - past;
-      // const rate = past === 0 ? 0 : net / past;
-      // growth = rate * 100;
-      // today = net;
+
+      // pres. |  past  |  net  |   rate  | result
+      // ------------------------------------------
+      //   100 |   100  |    0  |   0/100 |     0
+      //     0 |   100  | -100  |-100/100 |    -1
+      //   100 |     0  |  100  | 100/0   |   inf  → must return 1
+      //     0 |     0  |    0  |   0/0   |   inf  → must return 0
+
+      let present = value;
+      let past = source[index - 1][1];
+      let net = present - past;
+      let rate = 0;
+      // this is to handle the cases when past === 0
+      if (past === 0) {
+        rate = present > 0 ? 0.01 : 0;
+      } else {
+        rate = net / past;
+      }
+      growth = rate * 100;
+      today = net;
       // this is on the today's values
-      const present = value - source[index - 1][1];
-      const past = source[index - 1][1] - source[index - 2][1];
-      const net = present - past;
-      const rate = past === 0 ? 0 : net / past;
-      growth = rate < 1 ? rate * 100 : 100;
-      today = present;
+      // let present = value - source[index - 1][1];
+      // let past = source[index - 1][1] - source[index - 2][1];
+      // let net = present - past;
+      // let rate = past === 0 ? 0 : net / past;
+      // growth = rate < 1 ? rate * 100 : 100;
+      // today = present;
+      // debug
     }
     outputArray.push({ date: key, total: value, today, growth });
   });
